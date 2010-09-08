5d26fff6015e7d032dce92648e2737c34a69db7a
volker.richter <volker.richter@gmx.de>
2010-09-08 15:04:22 +0000

    Update Scheduling Container

--- branches/tud/vr/wimax/scheduling/virtualallocation.cc
+++ branches/tud/vr/wimax/scheduling/virtualallocation.cc
@@ -28,39 +28,66 @@ VirtualAllocation::~VirtualAllocation()
 /*
  * Adds a new Virtual Allocation in the Map
  */
-void VirtualAllocation::addAllocation( int cid)
-{
-
-    // Create new Virtual AllocationElement with 0 bytes and 0 slots
-    VirtualAllocationElement newAllocationElement( cid, 0, 0);
-
-    // Insert new Element into the map
-    virtualAllocationMap_.insert ( pair<int,VirtualAllocationElement>( cid, newAllocationElement) );
-}
-
-/*
- * Adds a new Virtual Allocation in the Map
- */
-void VirtualAllocation::addAllocation( int cid, u_int32_t wantedMstrSize, u_int32_t wantedMrtrSize, int nbOfSlots, int nbOfBytes)
+void VirtualAllocation::addAllocation( Connection* connection, u_int32_t wantedMstrSize, u_int32_t wantedMrtrSize, int slotCapacity, int nbOfSlots, int nbOfBytes)
 {
 
     // Create new Virtual Allocation Element
-    VirtualAllocationElement newAllocationElement( cid, wantedMstrSize, wantedMrtrSize, nbOfSlots, nbOfBytes);
+    VirtualAllocationElement newAllocationElement( connection, wantedMstrSize, wantedMrtrSize, slotCapacity, nbOfSlots, nbOfBytes);
 
     // Insert new Element into the map
-    virtualAllocationMap_.insert ( pair<int,VirtualAllocationElement>( cid, newAllocationElement) );
+    virtualAllocationMap_.insert ( pair< Connection*, VirtualAllocationElement>( connection, newAllocationElement) );
 }
 
 /*
  * Returns true if an Entry of this CID exits and set the current Iterator
  */
-bool VirtualAllocation::findCidEntry(int cid)
+bool VirtualAllocation::findConnectionEntry( Connection* connection)
 {
 
-    mapIterator_ = virtualAllocationMap_.find( cid);
+    mapIterator_ = virtualAllocationMap_.find( connection);
     return virtualAllocationMap_.end() != mapIterator_;
 
 }
+
+/*
+ * Return true if the first Entry has a connection and set the Iterator
+ */
+bool VirtualAllocation::firstConnectionEntry()
+{
+	mapIterator_ = virtualAllocationMap_.begin();
+	return virtualAllocationMap_.end() != mapIterator_;
+}
+
+/*
+ * Returns true if next Entry exists and set the Iterator
+ * If the end is reached the Iterator is set to the next Element
+ */
+bool VirtualAllocation::nextConnectionEntry()
+{
+	++mapIterator_;
+	if ( virtualAllocationMap_.end() != mapIterator_ ) {
+		return true;
+	} else {
+		// TODO: May cause problems if no Map Entry exists
+		mapIterator_ = virtualAllocationMap_.end();
+		return false;
+	}
+}
+
+
+/*
+ * Returns the slot capacity in byte of the connection
+ */
+int VirtualAllocation::getSlotCapacity()
+{
+	if ( virtualAllocationMap_.end() != mapIterator_ ) {
+		return mapIterator_->second.getSlotCapacity();
+	} else {
+	    fprintf(stderr,"ERROR: Iterator not valid use findCidEntry() before");
+	    exit(5);
+	}
+}
+
 
 /*
  * Returns the number of allocated slots for the current connection
@@ -70,6 +97,20 @@ int VirtualAllocation::getCurrentNbOfSlots()
 
     if ( virtualAllocationMap_.end() != mapIterator_ ) {
         return mapIterator_->second.getNbOfAllocatedSlots();
+    } else {
+        fprintf(stderr,"ERROR: Iterator not valid use findCidEntry() before");
+        exit(5);
+    }
+}
+
+/*
+ * Set the number of allocated slots for the current connection
+ */
+void VirtualAllocation::setCurrentNbOfSlots( int nbOfSlots)
+{
+
+    if ( virtualAllocationMap_.end() != mapIterator_ ) {
+        mapIterator_->second.setNbOfAllocatedSlots( nbOfSlots);
     } else {
         fprintf(stderr,"ERROR: Iterator not valid use findCidEntry() before");
         exit(5);
@@ -89,6 +130,21 @@ int VirtualAllocation::getCurrentNbOfBytes()
         exit(5);
     }
 }
+
+/*
+ * Returns the number of allocated bytes for the current connection
+ */
+void VirtualAllocation::setCurrentNbOfBytes( int nbOfBytes)
+{
+
+    if ( virtualAllocationMap_.end() != mapIterator_ ) {
+        mapIterator_->second.setNbOfAllocatedBytes( nbOfBytes);
+    } else {
+        fprintf(stderr,"ERROR: Iterator not valid use findCidEntry() before");
+        exit(5);
+    }
+}
+
 
 /*
  * Update values of the current virtual allocation
