1054086be88d3971c593158ddd4b7513e5704e78
volker.richter <volker.richter@gmx.de>
2011-01-11 15:06:08 +0000

    patch gcc-4.5

--- branches/tud/ns-2.34/mobile/nakagami.cc
+++ branches/tud/ns-2.34/mobile/nakagami.cc
@@ -180,9 +180,13 @@ double Nakagami::Pr(PacketStamp *t, PacketStamp *r, WirelessPhy *ifp)
  		double resultPower;
  		
         if (int_m == m) {
- 			resultPower = ErlangRandomVariable::ErlangRandomVariable(Pr/m, int_m).value();
+ 			// vr@tud gcc-4.5
+        	// resultPower = ErlangRandomVariable::ErlangRandomVariable(Pr/m, int_m).value();
+ 			resultPower = ErlangRandomVariable(Pr/m, int_m).value();
  		} else {
- 			resultPower = GammaRandomVariable::GammaRandomVariable(m, Pr/m).value();
+ 			// vr@tud gcc-4.5
+ 			// resultPower = GammaRandomVariable::GammaRandomVariable(m, Pr/m).value();
+ 			resultPower = GammaRandomVariable(m, Pr/m).value();
  		}
  		return resultPower;
 	}
