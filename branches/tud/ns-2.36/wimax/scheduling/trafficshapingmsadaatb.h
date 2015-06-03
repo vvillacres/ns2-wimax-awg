57679bd4330fb6d03acf5c639c578b245095ebe9
volker.richter <volker.richter@gmx.de>
2015-06-03 14:48:43 +0000

    Traffic Shaping Alorigthms from Msadaa

---
+++ branches/tud/ns-2.36/wimax/scheduling/trafficshapingmsadaatb.h
@@ -0,0 +1,58 @@
+/*
+ * trafficshapingmsadaatb.h
+ *
+ *  Created on: 01.06.2015
+ *      Author: richter
+ */
+
+#ifndef TRAFFICSHAPINGMSADAATB_H_
+#define TRAFFICSHAPINGMSADAATB_H_
+
+#include "trafficshapinginterface.h"
+#include <map>
+#include <deque>
+using namespace std;
+
+
+
+
+//----------------Prediction Data-------------------------------
+
+struct LastBucketSize {
+    u_int32_t lastCirToken;
+    u_int32_t lastEirToken;
+    double   timeStamp;
+};
+
+
+typedef map< int, LastBucketSize > MapLastBucketSize_t;
+typedef MapLastBucketSize_t::iterator LastBucketSizeIt_t;
+
+class TrafficShapingMsadaaTb: public TrafficShapingInterface
+{
+public:
+    TrafficShapingMsadaaTb( double frameDuration);
+    virtual ~TrafficShapingMsadaaTb();
+
+    /*
+     * Returns wantedMstrSize and wantedMrtrSize as guideline for the scheduling algorithm
+     * through call by reference
+     */
+    virtual MrtrMstrPair_t getDataSizes(Connection * connection, u_int32_t queuePayloadSize);
+
+    /*
+     * Occurred allocation is send back to the traffic policing algorithm to use this
+     * values in the next call
+     */
+    virtual void updateAllocation(Connection *connection,u_int32_t realMrtrSize,u_int32_t realMstrSize);
+
+private :
+    /*
+     * Saves values for usage in the next call
+     */
+    MapLastBucketSize_t mapLastBucketSize_ ;
+
+
+};
+
+#endif /* TRAFFICSHAPINGTSWTCM_H_ */
